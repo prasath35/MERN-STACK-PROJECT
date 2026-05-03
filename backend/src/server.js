93e20ed8e23c6c8b9c4e56110f4659e1f91f6333
prasath35 <prasath074335@gmail.com>
2026-05-03 19:11:12 +0530

    done sir

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,13 @@
-
 import express from "express";
 import { ENV } from "./lib/env.js";
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
-
+// Use Render's PORT fallback
+const PORT = process.env.PORT || ENV.PORT || 5000;
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "api is up and running" });
 });
 
-app.listen(ENV.PORT, () => console.log("server is running on port " + ENV.PORT));
+app.listen(PORT, () => console.log("server is running on port " + PORT));
