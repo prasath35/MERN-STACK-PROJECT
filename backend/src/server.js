47d0eff7e13da66817c601d8f1ec757f632d2a71
prasath35 <prasath074335@gmail.com>
2026-05-05 15:06:11 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,23 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import cors from "cors";
+import {serve} from "inngest/express";
+
 
 const app = express();
 
 // Use Render's PORT fallback
 const PORT = process.env.PORT || ENV.PORT || 5000;
+
+
+//midlleware
+app.use(express.json());
+
+//credentials:true meaning?? => server allows a browser to incluide cookies on requests
+app.use(cors({origin: ENV.CLIENT_URL,credentials:true}));
+
+
+app.use("/api/inngest", serve({client:inngest, fucntions}));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "api is up and running" });
